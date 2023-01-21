3cf4541e9627eeab719e5f1ccfc265713049ff7a
orion <orion_alzate@hotmail.com>
2023-01-20 20:50:17 -0500

    cancion y lancher realizados

--- app/src/main/java/com/example/turistiando/Splash.java
+++ app/src/main/java/com/example/turistiando/Splash.java
@@ -1,13 +1,47 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity {
 
+    // Atributos
+    MediaPlayer cancion;
+
+    //Metodos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Oculte el action bar
+        ActionBar barra = getSupportActionBar();
+        barra.hide();
+
+        //Lanzar la cancion
+        cancion = MediaPlayer.create(this,R.raw.song);
+        cancion.start();
+
+        // Temporizador para la cancion
+        TimerTask inicioapp = new TimerTask() {
+            @Override
+            public void run() {
+
+                // lanzar una nueva actividad
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicioapp, 10000);
+
     }
+
 }
