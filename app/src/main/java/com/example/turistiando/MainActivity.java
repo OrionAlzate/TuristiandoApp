437a01b3217affc613b9d51b2fade77ef574c2a8
moises hernandez <moiseslds1234@gmail.com>
2023-01-27 19:15:58 -0500

    Metodos para cambio de idioma

--- app/src/main/java/com/example/turistiando/MainActivity.java
+++ app/src/main/java/com/example/turistiando/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.turistiando;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
@@ -10,6 +11,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +24,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // cambiar color del actionBar
         ActionBar barra = getSupportActionBar();
-        // cambiando el color del actionbar
+        // asignar un color el color del actionbar
         ColorDrawable colorBarra = new ColorDrawable(Color.parseColor("#ed7607"));
         barra.setBackgroundDrawable(colorBarra);
 
 
     }
+
+    //Metodo para cambiar el idioma de mi app
+    public void cambiarIdioma(String idioma){
+
+        //configurando el lenguaje del telefono
+        Locale lenguaje = new Locale(idioma);
+        //Asignar el idioma
+        Locale.setDefault(lenguaje);
+
+        //configuracion en el telefono
+        Configuration configuracionTelefono = getResources().getConfiguration();
+        configuracionTelefono.locale = lenguaje;
+
+        //Ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono, getBaseContext().getResources().getDisplayMetrics());
+
+
+    }
+
 
     //metodo de creacion del menu que creamos con xml
     public boolean onCreateOptionsMenu(Menu menudo) {
@@ -44,16 +67,24 @@ public class MainActivity extends AppCompatActivity {
 
         switch (itemSeleccionado){
             case(R.id.option1):
-                Toast.makeText(this, "opcion 1", Toast.LENGTH_SHORT).show();
+                
                 Intent intent = new Intent(getApplicationContext(),AcercaDe.class);
                 startActivity(intent);
 
                 break;
             case(R.id.option2):
-                Toast.makeText(this, "opcion 2", Toast.LENGTH_SHORT).show();
+
+                this.cambiarIdioma("en");
+                Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent1);
+
                 break;
             case(R.id.option3):
-                Toast.makeText(this, "opcion 3", Toast.LENGTH_SHORT).show();
+
+                this.cambiarIdioma("es");
+                Intent intent2 = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent2);
+
                 break;
         }
 
